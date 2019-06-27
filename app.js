2aec9f99facaa024488306405b83a673baf8675f
Dragomir <svedava@mail.ru>
2019-06-27 22:33:12 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const adminHandlers = require("./routes/admin");
+const shopHandlers = require("./routes/shop");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(adminHandlers);
+app.use(shopHandlers);
+
+app.use("/", (req, res) => {
+	res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.listen(3000);
