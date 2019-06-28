e3af725cdfc9e99f680096fab857e6167d5b1b1c
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-06-28 17:03:06 +0100

    converted html to handlebars

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const path = require("path");
-const adminHandlers = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopHandlers = require("./routes/shop");
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminHandlers);
+app.use("/admin", adminData.routes);
 app.use(shopHandlers);
 
 app.use("/", (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
